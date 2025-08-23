c6b1b50a7d5b35d380a7228a2b418fa529b188a0
EH-Cyber <54258604+EH-cyber@users.noreply.github.com>
2025-08-22 20:11:11 -0400

    Created toBinary()
    Created function to convert decimal numbers to binary

--- newBinary.py
+++ newBinary.py
@@ -15,57 +15,10 @@ def main():
 	match decision:
 		# Decimal to binary conversion
 		case 1:
-			DecNum = input('What is the Decimal number you would like to convert?\n')
-			DecNum = float(DecNum)
-			DecList = math.modf(DecNum)
-			DecListFrac = DecList[0]
-			DecListWhole = DecList[1]
-			BinWholConv = []
-			# Whole number to binary conversion
-			while DecListWhole > 0:
-				BinNum = DecListWhole % 2
-				BinNum = int(BinNum)
-				if BinNum == 0:
-					BinWholConv.append(0)
-				elif BinNum == 1:
-					BinWholConv.append(1)
-				DecListWhole = DecListWhole / 2
-				WholeTup = math.modf(DecListWhole)
-				DecListWhole = int(WholeTup[1])
-			WholeConvStr = str(BinWholConv)
-			cleanWholeConv = cleanStr(WholeConvStr, 1)
-			# cleanWholeConv = WholeConvStr.strip('[]')
-			# cleanWholeConv = cleanWholeConv.replace(',', '')
-			# cleanWholeConv = cleanWholeConv.replace(' ', '')
-			# cleanWholeConv = cleanWholeConv[::-1]
-			# print('Whole Num ', DecList, ' is ', cleanWholeConv[::-1]) !! For TESTING
-			# Fraction to binary conversion
-			BinFracConv = []
-			BinFracLength = len(BinFracConv)
-			while DecListFrac > 0 and BinFracLength < 8:
-				BinNum = DecListFrac * 2
-				BinNum = float(BinNum)
-				if BinNum < 1 and BinNum > 0:
-					BinFracConv.append(0)
-					DecListFrac = BinNum
-					# print(DecListFrac) !! For TESTING
-				elif BinNum >= 1:
-					BinFracConv.append(1)
-					DecListFrac = BinNum - 1
-				FracConvStr = str(BinFracConv)
-				BinFracLength = len(BinFracConv)
-				cleanFracConv = cleanStr(FracConvStr, 0)
-				# cleanFracConv = FracConvStr.strip('[]')
-				# cleanFracConv = cleanFracConv.replace(',', '')
-				# cleanFracConv = cleanFracConv.replace(' ', '')
-			if BinWholConv:
-				if BinFracConv:
-					print('The Binary of ', DecNum, ' is ', cleanWholeConv + '.' + cleanFracConv)
-				else:
-					print(cleanWholeConv)
-			elif cleanFracConv:
-				print('The Binary of ', DecNum, ' is 0.' + cleanFracConv)
-
+			# Request input and convert it to float
+			convNum = float(input('What decimal number would you like to convert to binary? \nNumber to convert: '))
+			newNum = toBinary(convNum)
+			print(f'{convNum} in binary is {newNum}')
 
 		# Binary to Decimal conversion
 		case 2:
@@ -119,11 +72,17 @@ def main():
 			splitFracDec = FracDec.split('.')
 			cleanFracDec = splitFracDec[1]
 			print('The Decimal equivalent of ', BFStr, ' is ', str(WholDec) + '.' + cleanFracDec)
+		# Catch all inappropriate input
+		case _:
+			print(f'{decision} is not a choice please restart the program.')
 
 
 def cleanStr(dirtyNum, fracWhole):
 	'''
-	cleanStr is for cleaning the string from the 
+	cleanStr is for cleaning the string to display the appropriate binary number
+	input for cleanStr() is:
+	dirtyNum: String from binary list
+	fracWhole: 1 = Whole Number / 0 = Fraction of Number 
 	'''
 	cleanNum = dirtyNum.strip('[]')
 	cleanNum = cleanNum.replace(',','')
@@ -131,6 +90,65 @@ def cleanStr(dirtyNum, fracWhole):
 	if fracWhole == 1:
 		cleanNum = cleanNum[::-1]
 	return cleanNum
+
+def toBinary(decNum):
+	'''
+	Function to convert Decimal to Brinary
+	Takes input of decNum which must be a number
+	'''
+
+	# math.modf() converts number to list of what is before and after the decimal
+	DecList = math.modf(decNum)
+	DecListFrac = DecList[0]
+	DecListWhole = DecList[1]
+	# Empty List to build binary number
+	BinWholConv = []
+
+	# Whole number to binary conversion
+	while DecListWhole > 0:
+		BinNum = DecListWhole % 2
+		BinNum = int(BinNum)
+		if BinNum == 0:
+			BinWholConv.append(0)
+		elif BinNum == 1:
+			BinWholConv.append(1)
+		DecListWhole = DecListWhole / 2
+		WholeTup = math.modf(DecListWhole)
+		DecListWhole = int(WholeTup[1])
+
+	# Convert BinWholConv from List to String
+	WholeConvStr = str(BinWholConv)
+	# Clean String
+	cleanWholeConv = cleanStr(WholeConvStr, 1)
+
+	# Fraction to binary conversion
+	BinFracConv = []
+	BinFracLength = len(BinFracConv)
+	while DecListFrac > 0 and BinFracLength < 8:
+		BinNum = DecListFrac * 2
+		BinNum = float(BinNum)
+		if BinNum < 1 and BinNum > 0:
+			BinFracConv.append(0)
+			DecListFrac = BinNum
+			# print(DecListFrac) !! For TESTING
+		elif BinNum >= 1:
+			BinFracConv.append(1)
+			DecListFrac = BinNum - 1
+		FracConvStr = str(BinFracConv)
+		BinFracLength = len(BinFracConv)
+		cleanFracConv = cleanStr(FracConvStr, 0)
+
+	# Determines what to return
+	if BinWholConv:
+		if BinFracConv:
+			numStr = cleanWholeConv + '.' + cleanFracConv
+			return numStr
+		else:
+			return cleanWholeConv
+	elif cleanFracConv:
+		numStr = '0.' + cleanFracConv
+		return numStr
+
 
 # Run the main function (AKA run program)
 main()
